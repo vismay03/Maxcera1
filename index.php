fced583cb47bf9ec5f7016aa071672c0bccd0383
MihirPatel2310 <patelmihir2310@gmail.com>
2020-12-06 13:28:23 +0530

    about us

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,34 @@
     <link rel="stylesheet" href="css/style.css">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
+
 <body>
     <!-- NAVBAR -->
     <?php
-        include('comp/header.php');
+    include('comp/header.php');
     ?>
 
-    
+    <section>
+        <img src="images/landingImg.png" alt="" />
+    </section>
+
+    <div class="flex mx-auto about py-24">
+        <img src="images/aboutImg.png" class="about-img" alt="" />
+        <div class="about-content self-center text-white">
+            <h2 class=" text-5xl uppercase">About us</h2>
+            <p class="text-sm mt-8" >SOCCA SanitaryWares is a modernized enterprise integrated with R&D, manufacturing, sales and services, having a complete set of integrated sales team, applying brand strategy. Setting up enterprise image, and being the advocate and Pioneer in the field of Ceramics.
+            </p>
+            <p class="text-sm mt-3" >We are feeling very happy to introduce our new venture SOCCA SANITARYWARE with a wide range of SanitaryWare products and accessories.
+            </p>
+        </div>
+    </section>
 
 
+
+
+        <!-- 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="js/activeClass.js"></script>
+   -->
 </body>
+
 </html>
