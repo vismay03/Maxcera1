dfd8bb9f10b50c32f53aeb5eb905248cf6835850
MihirPatel2310 <patelmihir2310@gmail.com>
2020-12-07 22:34:36 +0530

    update

--- index.php
+++ index.php
@@ -27,22 +27,22 @@ header('Cache-Control: post-check=0, pre-check=0', false)
 
     <!-- ABOUT -->
 
-    <div class="flex justify-center about py-2">
+    <div class="flex justify-center about mt-16 py-2">
         <img src="images/aboutImg.png" class="about-img" alt="" />
         <div class="about-content self-center text-white">
-            <h2 class=" text-5xl uppercase">About us</h2>
-            <p class="text-lg mt-8">SOCCA SanitaryWares is a modernized enterprise integrated with R&D, manufacturing, sales and services, having a complete set of integrated sales team, applying brand strategy. Setting up enterprise image, and being the advocate and Pioneer in the field of Ceramics.
+            <h2 class="text-4xl uppercase text-">About us</h2>
+            <p class="text-md mt-8">SOCCA SanitaryWares is a modernized enterprise integrated with R&D, manufacturing, sales and services, having a complete set of integrated sales team, applying brand strategy. Setting up enterprise image, and being the advocate and Pioneer in the field of Ceramics.
             </p>
-            <p class="text-lg mt-3">We are feeling very happy to introduce our new venture SOCCA SANITARYWARE with a wide range of SanitaryWare products and accessories.
+            <p class="text-md mt-3">We are feeling very happy to introduce our new venture SOCCA SANITARYWARE with a wide range of SanitaryWare products and accessories.
             </p>
         </div>
     </div>
 
     <!-- COLLECTION -->
 
-    <div class="flex flex-col justify-center mx-auto collection pb-32">
-        <h2 class="text-5xl collection-title uppercase text-center"> Our COLLECTION</h2>
-        <div class="mt-12 flex flex-wrap justify-around">
+    <div class="flex flex-col justify-center mx-auto collection pb-16">
+        <h2 class="text-4xl collection-title uppercase text-center mt-16"> Our COLLECTION</h2>
+        <div class=" flex flex-wrap justify-around">
             <section class="flex flex-col w-auto justify-center mt-16">
                 <img src="images/aboutImg.png" class="self-center" loading="lazy" alt="" />
                 <h3 class="uppercase text-white text-center text-2xl mt-8">COmmodes</h3>
@@ -61,8 +61,8 @@ header('Cache-Control: post-check=0, pre-check=0', false)
     <!-- PRODUCTS -->
 
     <div class="flex flex-col justify-center mx-auto product pb-32">
-        <h2 class="text-5xl product-title uppercase text-center">Premium Products</h2>
-        <div class="mt-12 flex flex-col md:flex-row justify-around">
+        <h2 class="text-4xl product-title uppercase text-center">Premium Products</h2>
+        <div class=" flex flex-col md:flex-row justify-around">
             <section class="flex flex-col w-auto justify-center mt-16">
                 <img src="images/productImg.png" loading="lazy" class="self-center" alt="" />
 
@@ -79,7 +79,7 @@ header('Cache-Control: post-check=0, pre-check=0', false)
 
     <!-- COMPANY FACTS -->
 
-    <!-- <div class="company-facts flex flex-col sm:flex-row justify-around">
+    <div class="company-facts flex flex-col sm:flex-row justify-around">
         <section class="facts-content flex flex-wrap h-full justify-center w-full">
             <div class="sm:w-1/2 w-full self-center flex justify-center text-center">
 
@@ -114,22 +114,22 @@ header('Cache-Control: post-check=0, pre-check=0', false)
                     </section>
                 </div>
         </section>
-    </div> -->
+    </div>
 
     <!-- ENQUIRY -->
 
-    <div class="flex justify-around w-full flex-col sm:flex-row flex-wrap enquiry bg-black text-white py-6">
+    <div class="flex justify-around w-full flex-col sm:flex-row flex-wrap-reverse enquiry bg-black text-white py-6">
         <section class="text-center">
             <h2>Have Questions? Call Us: +91 90161 61636</h2>
             <p>Or mail us on info@maxcerasanitarywear.com</p>
         </section>
         <a class="py-2 px-4 hover:text-white self-center mt-5 sm:mt-0" href="contact.php">Send An Enquiry</a>
     </div>
-
+    <hr>
     <!-- FOOTER -->
 
-    <footer class="flex justify-center pt-10 flex-col  sm:p-0 sm:w-full">
-        <div class="flex flex-col pl-5 sm:flex-row justify-around" >
+    <footer class="flex justify-center flex-col  sm:p-0 sm:w-full">
+        <div class="flex flex-col sm:flex-row justify-around pt-10" >
         <section class="text-white ">
         <img src="images/Maxcera-logo.png" class="logo mx-auto sm:mx-0" alt="logo" />
             <h3 class="mt-3 text-center sm:text-left" >Follow us on:</h3>
@@ -153,14 +153,14 @@ header('Cache-Control: post-check=0, pre-check=0', false)
 
             <section class="flex flex-col mt-3 sm:mt-0">
 
-                <h3 class="mt-3 flex items-end">
+                <h3 class="mt-4 flex items-end">
                     <svg width="20" height="20" class="self-end" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 6.49606C6 5.66924 6.67183 4.99738 7.5 4.99738C8.32817 4.99738 9 5.66924 9 6.49606C9 7.3229 8.32816 7.99478 7.5 7.99478C6.67184 7.99478 6 7.3229 6 6.49606Z" fill="#F2F2F2" />
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M1 6.49606C1 2.90877 3.91117 0 7.5 0C11.0888 0 14 2.90877 14 6.49606C14 9.1701 12.6617 11.2886 11.2284 12.7209C10.5102 13.4387 9.75773 13.9953 9.11325 14.3751C8.79117 14.5649 8.49017 14.7141 8.22849 14.8174C7.98144 14.9149 7.72211 14.9909 7.5 14.9909C7.27789 14.9909 7.01856 14.9149 6.77151 14.8174C6.50983 14.7141 6.20883 14.5649 5.88675 14.3751C5.24227 13.9953 4.48984 13.4387 3.77156 12.7209C2.33829 11.2886 1 9.1701 1 6.49606ZM7.5 3.99738C6.12017 3.99738 5 5.11633 5 6.49606C5 7.87579 6.12016 8.99478 7.5 8.99478C8.87984 8.99478 10 7.87579 10 6.49606C10 5.11633 8.87983 3.99738 7.5 3.99738Z" fill="#F2F2F2" />
                     </svg>
                     Maxcera Sanitaryware</h3>
 
-                <h2 class="text-xs w-64">
+                <h2 class="text-xs w-64 mt-1">
 
 
                     Shop No. 20,
@@ -168,22 +168,22 @@ header('Cache-Control: post-check=0, pre-check=0', false)
                     Trajpar, Morbi, Rajkot-363642, Gujarat, India
 
                 </h2>
-                <h3 class="mt-3 leading-none flex items-end ">
+                <h3 class="mt-4 leading-none flex items-end ">
                     <svg width="15" height="15" class="self-center mr-1" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2.5 0C1.11929 0 0 1.11929 0 2.5V4.5C0 10.299 4.70101 15 10.5 15H12.5C13.8807 15 15 13.8807 15 12.5V11.118C15 10.5499 14.679 10.0305 14.1708 9.77639L11.7549 8.56843C10.9384 8.1602 9.94971 8.56975 9.66105 9.43573L9.36328 10.329C9.25014 10.6684 8.90197 10.8705 8.55114 10.8003C6.35528 10.3612 4.63885 8.64472 4.19967 6.44886C4.12951 6.09803 4.33156 5.74986 4.67097 5.63672L5.7796 5.26718C6.52319 5.01932 6.95058 4.24075 6.76048 3.48035L6.17444 1.1362C6.0075 0.468446 5.40752 0 4.71922 0H2.5Z" fill="white" />
                     </svg>
                     Phone:
                 </h3>
-                <h2 class="text-xs mt-1">
+                <h2 class="text-xs mt-2">
                     +91 9090103010
                 </h2>
-                <h3 class="mt-3 leading-none flex items-end">
+                <h3 class="mt-4 leading-none flex items-end">
                     <svg width="20" height="15" class="self-end mr-1" viewBox="0 0 437 327" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M364.167 54.5H72.8337C52.8045 54.5 36.5991 66.7625 36.5991 81.75L36.417 245.25C36.417 260.237 52.8045 272.5 72.8337 272.5H364.167C384.196 272.5 400.584 260.237 400.584 245.25V81.75C400.584 66.7625 384.196 54.5 364.167 54.5ZM364.167 109L218.5 177.125L72.8337 109V81.75L218.5 149.875L364.167 81.75V109Z" fill="white" />
                     </svg>
                     email:
                     <h3>
-                        <h2 class="text-xs mt-1">
+                        <h2 class="text-xs mt-2">
                             maxcerasanitaryware
                         </h2>
 
