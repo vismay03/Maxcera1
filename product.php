2dd34a28eb771cf2137fce3b5f28008658fdeae7
vismay <vismaykatharani03@gmail.com>
2020-12-10 20:14:58 +0530

    title update

--- product.php
+++ product.php
@@ -20,18 +20,22 @@
 
   <!-- TITLE -->
 
-  <div class="title text-white flex flex-wrap py-20 items-center justify-around">
+  <div class="title text-white flex flex-col sm:flex-row flex-wrap py-20 items-center justify-around">
 
     <!-- CATEGORY -->
     <section class="category relative">
-      <h2 class="uppercase text-4xl text-center py-5 font-bold">COMMODES</h2>
+      <h2 class="uppercase text-4xl text-center pb-16 font-bold">COMMODES</h2>
       <img src="images/commod.png" width="200" height="300" alt="" />
     </section>
 
     <!-- TABS -->
 
-    <div class="mt-8 md:mt-16 sm:mt-0 block">
-      <nav class="tabs flex flex-col sm:flex-row">
+    <div class="mt-16 tabs-container md:mt-16 sm:mt-0">
+      <input type="checkbox" class="opacity-0 absolute mb-8" name="accordian-btn" id="btn" />
+
+    <label for="btn" class="block cursor-pointer sm:hidden text-2xl  uppercase py-2 px-5" >Commodes</label>
+     
+      <nav class="tabs flex overflow-hidden mt-5 flex-col sm:flex-row">
         <button data-target="panel-1" class="tab active uppercase py-4 px-6 block focus:outline-none text-white font-medium">
           double color one piece
         </button><button data-target="panel-2" class="tab uppercase ext-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none">
@@ -63,7 +67,7 @@
 
               <div class="product-box mt-6 sm:mt-0">
                 <a class="relative rounded-t product-link">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48 hover:scale-110" src="images/commod.png">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -73,8 +77,8 @@
               </div>
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -84,8 +88,8 @@
               </div>
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -95,8 +99,8 @@
               </div>
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
