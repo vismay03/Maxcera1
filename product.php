acec4757fbc9bd24cb37beb9859f230a0747861d
vismay <vismaykatharani03@gmail.com>
2020-12-10 23:39:36 +0530

    update

--- product.php
+++ product.php
@@ -6,10 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Product</title>
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/header.css">
-  <link rel="stylesheet" href="css/footer.css">
-  <link rel="stylesheet" href="css/product.css">
-
+  <!-- <link rel="stylesheet" href="css/header.css"> -->
+  <link rel="preload" as="style" href="css/footer.css" onload="this.rel='stylesheet'" />
+  <link rel="preload" as="style" href="css/product.css" onload="this.rel='stylesheet'" />
+  <link rel="preload" as="style" href="css/header.css" onload="this.rel='stylesheet'" />
 </head>
 
 <body>
@@ -33,8 +33,8 @@
     <div class="mt-16 tabs-container md:mt-16 sm:mt-0">
       <input type="checkbox" class="opacity-0 absolute mb-8" name="accordian-btn" id="btn" />
 
-    <label for="btn" class="block cursor-pointer sm:hidden text-2xl  uppercase py-2 px-5" >Commodes</label>
-     
+      <label for="btn" class="block cursor-pointer sm:hidden text-2xl  uppercase py-2 px-5">Commodes</label>
+
       <nav class="tabs flex overflow-hidden mt-5 flex-col sm:flex-row">
         <button data-target="panel-1" class="tab active uppercase py-4 px-6 block focus:outline-none text-white font-medium">
           double color one piece
@@ -126,8 +126,8 @@
             <div class="flex flex-wrap w-4/5 mx-auto justify-around -m-4 ">
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -137,8 +137,8 @@
               </div>
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -148,8 +148,8 @@
               </div>
 
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -157,7 +157,6 @@
                   <p class="mt-1 text-sm font-medium"> <span class="font-semibold">Size: 200 x 300</span></p>
                 </div>
               </div>
-
 
 
             </div>
@@ -176,9 +175,9 @@
           <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap w-4/5 mx-auto justify-around -m-4 ">
 
-              <div class="product-box  mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+              <div class="product-box mt-6 sm:mt-0">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -186,10 +185,9 @@
                   <p class="mt-1 text-sm font-medium"> <span class="font-semibold">Size: 200 x 300</span></p>
                 </div>
               </div>
-
               <div class="product-box mt-6 sm:mt-0">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
@@ -216,9 +214,9 @@
           <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap w-4/5 mx-auto justify-around -m-4 ">
 
-              <div class="product-box">
-                <a class="relative rounded-t product-link overflow-hidden">
-                  <img alt="ecommerce" class="object-contain rounded-t object-center h-48" src="images/commod.png">
+              <div class="product-box mt-6 sm:mt-0">
+                <a class="relative rounded-t product-link">
+                  <img alt="ecommerce" class="object-contain rounded-t object-center h-64 hover:scale-110" src="images/commod.png">
                 </a>
                 <div class="product-text py-2 pl-2">
                   <h3 class="text-sm tracking-widest font-medium title-font mb-1"> <span class="font-semibold">Name: NAME</span></h3>
