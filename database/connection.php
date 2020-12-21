6b6cd43560be37329e22a56cb8c14720ac752e86
vismay <vismaykatharani03@gmail.com>
2020-12-21 21:18:27 +0530

    update

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
 
-// $connection = new  mysqli("localhost", "u588429006_Maxcera", "Maxcera159357", "u588429006_Maxcera");
-  $connection = new  mysqli("localhost", "root", "", "Maxcera");
+$connection = new  mysqli("localhost", "u588429006_Maxcera", "Maxcera159357", "u588429006_Maxcera");
+//   $connection = new  mysqli("localhost", "root", "", "Maxcera");
 if ($connection->connect_error) {
     die('Connection Failed ' . $connection->connect_error);
 }
